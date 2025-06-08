d450cebd4b329e1002e880f9c406a66b361bf586
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2025-06-08 19:18:40 -0300

    desafio para implementar as funções de multiplicar e dividir da calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: rgb(191, 191, 191);
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+  font-family: "Roboto";
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color:rgb(191, 191, 191);
+    border: 0;
+    float: right;
+    clear: both;
+    display: flex;
+    flex-direction: colunm;
+    align-items: flex-end;
+    padding: 0 10px;
+    font-size: 24px;
+    font-family: "Roboto";
+  }
+`;
